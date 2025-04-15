c5027022f6dc0cc332fbb59462434f1d6ebc972c
Jin <thdwlsgus0@naver.com>
2025-04-15 21:55:20 +0900

    style: 메인페이지 작업

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,34 @@
+'use client'
+
 import { QRCodeSVG } from 'qrcode.react';
-import styles from "./page.module.scss";
+import styles from './page.module.scss';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <h1>QR Code Example</h1>
-      <div className={styles.qrCode}>
-        <QRCodeSVG
-          value="https://www.naver.com"
-          size={200}
-          level="H"
-          includeMargin={true}
-          bgColor="#ffffff"
-          fgColor="#000000"
-        />
-      </div>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h1>ELRC 출석 체크</h1>
+        <p>Create your own QR code in seconds</p>
+      </header>
+
+      <main className={styles.main}>
+        <div className={styles.qrSection}>
+          <div className={styles.qrContainer}>
+            <QRCodeSVG
+              value="https://www.naver.com"
+              size={200}
+              level="H"
+              includeMargin={true}
+              bgColor="#ffffff"
+              fgColor="#000000"
+            />
+          </div>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <p>© 2025 QR Code Generator. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
