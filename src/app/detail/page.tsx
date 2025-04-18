770a215821bd61f0b8f4a67823d559e024f9d200
Jin <thdwlsgus0@naver.com>
2025-04-18 21:34:00 +0900

    fix: modify Suspense

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -3,45 +3,50 @@
 import { QRCodeSVG } from 'qrcode.react';
 import { useSearchParams } from 'next/navigation';
 import styles from './page.module.scss';
-import { useGetForm } from '../../common/hooks/useGetForm';
+import { useGetForm } from '@/common/hooks/useGetForm';
 import { Suspense } from 'react';
 
-export default function MainPage() {
+function DetailContent() {
   const searchParams = useSearchParams();
   const formId = searchParams.get('id') || '';
   const { data: formData } = useGetForm(formId);
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <div className={styles.container}>
-        <header className={styles.header}>
-          <h1>ELRC 출석 체크</h1>
-          <p>Create your own QR code in seconds</p>
-        </header>
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <h1>ELRC 출석 체크</h1>
+        <p>Create your own QR code in seconds</p>
+      </header>
 
-        <main className={styles.main}>
-          <div className={styles.qrSection}>
-            <div className={styles.qrContainer}>
-              {/* <div>
-                <img src={dog?.message} alt="dog" />
-              </div> */}
-              <QRCodeSVG
-                value={formData?.formUrl || ''}
-                size={200}
-                level="H"
-                includeMargin={true}
-                bgColor="#ffffff"
-                fgColor="#000000"
-              />
-            </div>
+      <main className={styles.main}>
+        <div className={styles.qrSection}>
+          <div className={styles.qrContainer}>
+            {/* <div>
+              <img src={dog?.message} alt="dog" />
+            </div> */}
+            <QRCodeSVG
+              value={formData?.formUrl || ''}
+              size={200}
+              level="H"
+              includeMargin={true}
+              bgColor="#ffffff"
+              fgColor="#000000"
+            />
           </div>
-        </main>
+        </div>
+      </main>
 
-        <footer className={styles.footer}>
-          <p>© 2025 QR Code Generator. All rights reserved.</p>
-        </footer>
-      </div>
+      <footer className={styles.footer}>
+        <p>© 2025 QR Code Generator. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
+
+export default function DetailPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <DetailContent />
     </Suspense>
-
   );
 }
